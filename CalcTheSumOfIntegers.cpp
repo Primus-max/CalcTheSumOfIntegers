1a6b1f3d7049ef7915648b0ec18ff514fb4126b3
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-03 13:56:13 +0300

    Добавьте файлы проекта.

---
+++ CalcTheSumOfIntegers.cpp
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая вычисляет сумму целых
+// чисел от а до 500 (значение a вводится с клавиатуры).
+
+#include <iostream>
+#include<windows.h>
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(12510);
+    SetConsoleOutputCP(1251);    
+
+    int userInputA;
+    cout << "Помогу найти сумму целых чисел от а до 500, введите чему равно а: " ;
+    cin >> userInputA;
+
+    // Проверка на корректность ввода
+    while (true) 
+    {
+        // Проверка на корректность введенного значения a
+        if (userInputA > 500) 
+            cout << "Некорректное значение a. Введите значение от a до 500, (a  не может быть больше 500)" << endl;
+        else
+            break; // Возврат кода ошибки
+        
+   }
+
+    int accum = 0;
+    // Вычисление суммы целых чисел от a до 500
+    for (int i = userInputA; i <= 500; ++i) {
+        accum += i;
+    }
+
+    cout << "Сумма целых чисел от " << userInputA  << " до 500: " << accum << endl;
+
+}
